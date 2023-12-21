d5c078992b9c04649f855a4cbbda42a2801c04a2
xanderyeng <alexander.chepkiyeng@gmail.com>
2023-12-21 07:26:56 -0800

    Updates

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,1 +1,5 @@
-# Puppet script that extend the limit of results from 15 to 1024.
+# script which makes it possible to login with the holberton user and open file without error
+exec {'open any files without limits':
+  command => 'sed -rie \'s/(holberton (hard|soft) nofile).*/\1 1000/gi\' /etc/security/limits.conf',
+  path    => 'usr/bin/:/bin/'
+}
